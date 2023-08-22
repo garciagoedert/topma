85aa961dcb09dfd24be95828817316f2827f115f
Paulo Goedert <goedert@MacBook-Pro-de-Paulo.local>
2023-08-21 23:53:46 -0300

    att

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,19 @@
-
 function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue, td, th, any;
-  input = document.getElementById('myInput');
+  var input, filter, table, tr, td, i, txtValue, th;
+  input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      // td = tr[i].getElementsByTagName("th")[0];
+      if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+          } else {
+              tr[i].style.display = "none";
+          }
+      }
   }
 }
