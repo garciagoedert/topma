41acf837fe43821e1b062dede5e4a5fce8b0e7fe
Paulo Goedert <goedert@MacBook-Pro-de-Paulo.local>
2023-10-26 20:14:05 -0300

    att

--- frequência/script.js
+++ frequência/script.js
@@ -1,27 +1,90 @@
-// Função para registrar a presença
-function registrarPresença() {
-    // Pega os dados do formulário
-    const nome = document.querySelector("input[name='nome']").value;
-    const matrícula = document.querySelector("input[name='matrícula']").value;
+// Declaração das variáveis
+let alunos = [];
+let nome = document.getElementById("nome");
+let adicionar = document.getElementById("adicionar");
 
-    // Incrementa a contagem de presenças
-    const contagem = document.querySelector("#contagem");
-    contagem.innerHTML = parseInt(contagem.innerHTML) + 1;
+// Função para adicionar um aluno
+adicionar.addEventListener("click", function() {
+  // Verifica se o nome do aluno está preenchido
+  if (nome.value === "") {
+    alert("O nome do aluno deve ser preenchido.");
+    return;
+  }
+
+  // Adiciona o aluno à lista
+  alunos.push({
+    nome: nome.value
+  });
+
+  // Atualiza a lista de alunos
+  updateListaAlunos();
+});
+
+// Função para atualizar a lista de alunos
+function updateListaAlunos() {
+  // Limpa a lista
+  const listaAlunos = document.querySelector(".alunos");
+  listaAlunos.innerHTML = "";
+
+  // Adiciona os alunos à lista
+  alunos.forEach(aluno => {
+    const li = document.createElement("li");
+    li.textContent = aluno.nome;
+    listaAlunos.appendChild(li);
+  });
 }
 
-// Chama a função ao clicar no botão "Registrar presença"
-document.querySelector("input[type='submit']").addEventListener("click", registrarPresença);
+// Inicializa a lista de alunos
+updateListaAlunos();
 
 
-// limpar preseça
-// Função para limpar a contagem de presenças
-function limparContagem() {
-    const contagem = document.querySelector("#contagem");
-    contagem.innerHTML = 0;
+// Declaração das variáveis
+let alunos = [];
+let nome = document.getElementById("nome");
+let adicionar = document.getElementById("adicionar");
+
+// Função para adicionar um aluno
+adicionar.addEventListener("click", function() {
+  // Verifica se o nome do aluno está preenchido
+  if (nome.value === "") {
+    alert("O nome do aluno deve ser preenchido.");
+    return;
+  }
+
+  // Adiciona o aluno à lista
+  alunos.push({
+    nome: nome.value
+  });
+
+  // Atualiza a lista de alunos
+  updateListaAlunos();
+});
+
+// Função para remover um aluno
+function remover(aluno) {
+  // Remove o aluno da lista
+  alunos.splice(alunos.indexOf(aluno), 1);
+
+  // Atualiza a lista de alunos
+  updateListaAlunos();
 }
 
-// Chama a função ao clicar no botão "Limpar"
-document.querySelector("button").addEventListener("click", limparContagem);
+// Função para atualizar a lista de alunos
+function updateListaAlunos() {
+  // Limpa a lista
+  const listaAlunos = document.querySelector(".alunos");
+  listaAlunos.innerHTML = "";
 
+  // Adiciona os alunos à lista
+  alunos.forEach(aluno => {
+    const li = document.createElement("li");
+    li.textContent = aluno.nome;
+    li.addEventListener("click", function() {
+      remover(aluno);
+    });
+    listaAlunos.appendChild(li);
+  });
+}
 
-
+// Inicializa a lista de alunos
+updateListaAlunos();
